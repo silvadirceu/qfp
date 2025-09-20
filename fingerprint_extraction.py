144b5f112e7fd457fdcbea3661b22ab278d54a6a
Luiz-Brandi <luiz.brandi@innovox.com.br>
2025-09-20 14:28:07 +0000

    updating fingerprint extraction script

--- fingerprint_extraction.py
+++ fingerprint_extraction.py
@@ -4,34 +4,131 @@ import glob
 import os
 import time
 from qfp import QueryFingerprint
+from qfp.db_mem import InMemoryQfpDB
+import json
+from pathlib import Path
+
+
+# filenames = glob.glob("/mnt/disk1/BAF/audio/references/*.wav")
+# inicio = 388
+# start = time.time()
+# total = len(filenames[inicio:447])
+
+# for idx, file in enumerate(filenames[inicio:447], start=inicio):
+#     filename = os.path.splitext(os.path.basename(file))[0]
+#     print(f"[{idx}/{447}] {filename}")
+#     fp_r = ReferenceFingerprint(file)
+#     fp_r.create("/mnt/disk1/BAF/qfp_features/references")
+
+
+# end = time.time()     # marca o tempo final
+# print(f"Tempo de execução: {end - start:.4f} segundos")
 
 
 
-filenames = glob.glob("C:/Users/luizf/github/innovox/references/baf/BAF/audio/references/*.wav")
+# ---------------QUERY------------------
 
-# db = QfpDB(db_path="baf_data.db")
-start = time.time()
-for file in filenames[447:]:
-    filename = os.path.splitext(os.path.basename(file))[0]
-    print(filename)
-    fp_r = ReferenceFingerprint(file)
-    fp_r.create("C:/Users/luizf/github/innovox/qfp/data/baf_references")
-    # db.store(fp_r, filename)  
+db = InMemoryQfpDB()
+
+# armazenar vários
+start_index = time.time()
+db.store_all_pickles_from_directory(r"/mnt/disk1/BAF/qfp_features/references")
+end_index = time.time()
+
+query_path = "/mnt/disk1/BAF/audio/queries/query_0001.wav"
+query_fp = QueryFingerprint(query_path)
+filename = os.path.splitext(os.path.basename(query_path))[0]
+
+extraction_start = time.time()
+query_fp.create()  # gera hashes, peaks, strongest
+extraction_end = time.time()
+search_start = time.time()
+matches = db.query(query_fp, vThreshold=0.05, e_radius=0.09)
+search_end = time.time()
 
 
-# filename = os.path.splitext(os.path.basename(filenames[438]))[0]
-# fp_r = ReferenceFingerprint(filenames[438])
-# fp_r.create()
-# print(filename)
+sorted_matches = sorted(matches, key=lambda m: m.vScore, reverse=True)
 
-end = time.time()     # marca o tempo final
-print(f"Tempo de execução: {end - start:.4f} segundos")
+indexing_time = end_index - start_index
+extraction_time = extraction_end - extraction_start
+# faiss_search_time = search_end - search_start  # se você quiser separar FAISS de "busca total"
+search_time = search_end - search_start
+
+# montar dicionário final
+output = {
+    "total_matches": len(sorted_matches),
+    "indexing_time": indexing_time,
+    "extraction_time": extraction_time,
+    # "faiss_search_time": faiss_search_time,
+    "search_time": search_time,
+    "matches": [m._asdict() for m in sorted_matches]
+}
+with open(f"{filename}.json", "w", encoding="utf-8") as f:
+    json.dump(output, f, indent=4)
 
 
-# fp_q = QueryFingerprint("kiss_pitched_up.mp3")
-# fp_q.create()
-# db.query(fp_q)
-# print(fp_q.matches)
+
+
+
+# db = InMemoryQfpDB()
+
+# # armazenar vários
+# start_index = time.time()
+# db.store_all_pickles_from_directory("/mnt/disk1/BAF/qfp_features/references") 
+# end_index = time.time()
+
+# # diretório onde estão os áudios de query
+# query_dir = Path("/mnt/disk1/BAF/audio/queries")
+
+
+# # diretório para salvar os resultados
+# output_dir = Path("data")
+# output_dir.mkdir(parents=True, exist_ok=True)
+
+# # procurar todos arquivos que começam com "query_" e terminar em .wav
+# query_files = sorted(query_dir.glob("query_*.wav"))
+
+# for query_path in query_files:
+#     filename = query_path.stem  # ex: "query_0002"
+
+#     query_fp = QueryFingerprint(str(query_path))
+
+#     extraction_start = time.time()
+#     query_fp.create()  # gera hashes, peaks, strongest
+#     extraction_end = time.time()
+
+#     search_start = time.time()
+#     matches = db.query(query_fp, vThreshold=0.05, e_radius=0.09)
+#     search_end = time.time()
+
+#     sorted_matches = sorted(matches, key=lambda m: m.vScore, reverse=True)
+
+#     indexing_time = end_index - start_index
+#     extraction_time = extraction_end - extraction_start
+#     search_time = search_end - search_start
+
+#     # montar dicionário final
+#     output = {
+#         "total_matches": len(sorted_matches),
+#         "indexing_time": indexing_time,
+#         "extraction_time": extraction_time,
+#         "search_time": search_time,
+#         "matches": [m._asdict() for m in sorted_matches]
+#     }
+
+#     # salvar com o mesmo nome do arquivo de query
+#     json_path = output_dir / f"{filename}.json"
+#     with open(json_path, "w", encoding="utf-8") as f:
+#         json.dump(output, f, indent=4)
+
+#     print(f"Processed {query_path} -> {filename}.json")
+
+
+# query_path = "/mnt/disk1/BAF/audio/queries/query_0001.wav"
+# query_fp = QueryFingerprint(query_path)
+
+# query_fp.create(pickle_dir="/mnt/disk1/BAF/qfp_features/queries")
+# print(query_fp.subfingerprints)
 
 
 
